f3e4b92eb503cbfea169b72c3d8087d2f4c144c7
VAN BOSSUYT Nicolas <nivb@odoo.com>
2023-09-12 11:20:17 +0200

    Update

--- install.sh
+++ install.sh
@@ -7,16 +7,18 @@ sudo pacman -S base-devel git fish feh sxiv fish pyenv \
                blueman noto-fonts-emoji xf86-video-amdgpu \
                xorg xdg-user-dirs xorg-xinit fzf zathura-pdf-mupdf \
                clang llvm nasm qemu-full pavucontrol solaar bluez \
-               brightnessctl i3 picom kitty discord ttf-firacode \
-               ttf-comic-mono-git noto-fonts stow dunst scrot \
+               brightnessctl i3 picom kitty discord \
+               noto-fonts stow dunst scrot \
                imagemagick tk dunst scrot pulsemixer
 
 chsh -s /bin/fish $USER
 
-echo "Installing dots files..."
-git clone https://github.com/sleepy-monax/Dots Workspace/Dots
-cd Workspace/Dots/packages
+echo "Cleaning up random useless stuff"
+cd
 rm ~/.bashrc
+
+echo "Installing dots files..."
+cd Dots/packages
 ./stow
 cd
 
@@ -25,9 +27,9 @@ git clone https://aur.archlinux.org/yay.git Workspace/Yay
 cd Workspace/Yay && makepkg -si
 yay -S yay
 cd
-rm Workspace/Yay
+rm -rf Workspace/Yay
 
 yay -S dmenu2 i3lock-color google-chrome visual-studio-code-bin \
-       adw-gtk3 github-cli xfce-polkit
+       adw-gtk3 github-cli xfce-polkit ttf-firacode ttf-comic-mono-git
 
 xdg-user-dirs-update
